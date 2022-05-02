fc392ae07c6027fa84f67149f41af8338977430b
Ismael Peccin <ismaelpeccin@gmail.com>
2022-05-02 10:27:58 -0300

    feat: banco de dados

--- Store.sql
+++ Store.sql
@@ -8,8 +8,8 @@ CREATE TABLE products (
     id INT NOT NULL auto_increment,
     name VARCHAR(30) NOT NULL,
     quantity INT NOT NULL,
-    cost_price FLOAT NOT NULL,
-    sale_price FLOAT NOT NULL,
+    sale_price DECIMAL(8, 2),
+	cost_price DECIMAL(8, 2),
     PRIMARY KEY(id)
 ) ENGINE=INNODB;
 
@@ -28,11 +28,8 @@ CREATE TABLE purchases (
 CREATE TABLE sales_products (
     sale_id INT NOT NULL,
     product_id INT NOT NULL,
-    price FLOAT NOT NULL,
     quantity INT NOT NULL,
-    FOREIGN KEY(price) 
-        REFERENCES products (sale_price),
-        ON DELETE CASCADE,
+    total DECIMAL(8,2),
     FOREIGN KEY (sale_id)
         REFERENCES sales (id)
         ON DELETE CASCADE,
@@ -44,11 +41,8 @@ CREATE TABLE sales_products (
 CREATE TABLE purchases_products (
     purchase_id INT NOT NULL,
     product_id INT NOT NULL,
-    price FLOAT NOT NULL,
     quantity INT NOT NULL,
-    FOREIGN KEY(price) 
-        REFERENCES products (cost_price),
-        ON DELETE CASCADE,
+    total DECIMAL(8,2),
     FOREIGN KEY (purchase_id)
         REFERENCES purchases (id)
         ON DELETE CASCADE,
@@ -59,23 +53,24 @@ CREATE TABLE purchases_products (
 
 SET SQL_SAFE_UPDATES = 0;
 
-INSERT INTO StoreManager.products (name, quantity, cost_price, sale_price) VALUES
+INSERT INTO Store.products (name, quantity, sale_price, cost_price) VALUES
     ("Cadeira", 5, 25.00, 35.00),
     ("Chocolate em pó", 20, 7.00, 12.00),
     ("Refrigerante", 30, 3.50, 4.80);
 
-INSERT INTO StoreManager.sales (date) VALUES
+INSERT INTO Store.sales (date) VALUES
     (NOW()),
     (NOW());
 
-INSERT INTO StoreManager.purchases (date) VALUES
-    (NOW()),
+INSERT INTO Store.purchases (date) VALUES
     (NOW());
 
-INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity, price) VALUES
+INSERT INTO Store.sales_products (sale_id, product_id, quantity, total) VALUES
     (1, 3, 10, 48.00),
     (2, 1, 2, 70.00);
-
-INSERT INTO StoreManager.purchases_products (purchase_id, product_id, quantity, price) VALUES
+    
+INSERT INTO Store.purchases_products (purchase_id, product_id, quantity, total) VALUES
     (1, 1, 5, 125.00);
-  
+    
+
+
