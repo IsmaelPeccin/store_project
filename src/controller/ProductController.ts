e007ccf7df49a82b0433adb7761158e2da2241d3
Ismael Peccin <ismaelpeccin@gmail.com>
2022-05-03 11:10:11 -0300

    feat: funções para listar por id e para cadastrar

--- src/controller/ProductController.ts
+++ src/controller/ProductController.ts
@@ -8,8 +8,29 @@ export default class ProductController {
     this.productService = new ProductService();
   }
 
-  async list(_req: Request, res: Response) {
+  list = async (_req: Request, res: Response) => {
     const allProducts = await this.productService.list();
     return res.status(200).json(allProducts);
   }
+
+  findById = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const product = await this.productService.findById(id);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    return res.status(200).json(product);
+  }
+
+  create = async (req: Request, res: Response) => {
+    const product = req.body;
+
+    const productCreated = await this.productService.create(product);
+    if (productCreated === true) {
+      return res.status(400).json({ message: 'Product already exists' });
+    }
+    res.status(201).json(productCreated);
+  };
 } 
