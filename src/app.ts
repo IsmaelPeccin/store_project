12bc7cf9d276c24ef64ec173ffb7878657ee2226
Ismael Peccin <ismaelpeccin@gmail.com>
2022-05-05 16:05:56 -0300

    feat: rota /purchases

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 require('express-async-errors');
 import { errorMiddleware } from './middlewares';
-import { productRouter, salesRouter } from './routes';
+import { productRouter, purchaseRouter, salesRouter } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productRouter);
 app.use('/sales', salesRouter);
+app.use ('/purchases', purchaseRouter);
 
 app.use(errorMiddleware);
 
