269de628c86a41603e9282dedbf8c203aa5fb493
Ismael Peccin <ismaelpeccin@gmail.com>
2022-05-05 02:03:30 -0300

    feat: rotas para listar todas as vendas e por id

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 require('express-async-errors');
 import { errorMiddleware } from './middlewares';
-import { productRouter } from './routes';
+import { productRouter, salesRouter } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productRouter);
+app.use('/sales', salesRouter);
 
 app.use(errorMiddleware);
 
